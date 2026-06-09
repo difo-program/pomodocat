f2268e06f12f06e085eeed2b5454e902dca926b5
difo-program <99665847+difo-program@users.noreply.github.com>
2026-06-08 22:49:23 -0400

    Implement Pomodoro timer with break options
    Added Pomodoro timer functionality with short and long breaks.

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const startBtn = document.querySelector(".btn-start");
 const pauseBtn = document.querySelector(".btn-pause");
 const resetBtn = document.querySelector(".btn-reset");
 
+let gifActive = false;
+let gifBreak = true;
+
 let myInterval;
 let state = true;
 let statePause = true;
 let stateReset = true;
+
+let pomodoro = true;
+const pomodoroBtn = document.querySelector(".btn-menu-timer");
+let shortBreak = true;
+const shortBreakBtn = document.querySelector(".btn-menu-short");
+let longBreak = true;
+const longBreakBtn = document.querySelector(".btn-menu-long");
+
+let pomoCat = document.querySelector(".image");
 
 const minuteDiv = document.querySelector(".minutes");
 const secondDiv = document.querySelector(".seconds");
@@ -19,6 +31,9 @@ secondDiv.textContent = secondsDefault;
 
 const sessionAmount = Number.parseInt(minuteDiv.textContent);
 let totalSeconds = sessionAmount * 60;
+
+
+//Timer
 
 const appTimer = () => {
 
@@ -44,9 +59,20 @@ const appTimer = () => {
             bells.play();
             clearInterval(myInterval);
         }
-      
+
+        if(gifBreak == false){
+          pomoCat.src = "./assets/pomocat-break.gif";
+
+        } else if(gifActive == false){
+          pomoCat.src = "./assets/pomocat-active.gif";
+
+        } else{
+          pomoCat.src = "./assets/pomocat-pause.png";
+        }
+
     };
     myInterval = setInterval(updateSeconds, 1000);
+
   } else {
     alert("Session has already started.");
   }
@@ -55,13 +81,18 @@ const appTimer = () => {
 
 startBtn.addEventListener("click", appTimer);
 
+
+
+
 // Pause Button
 
 const appPause = () =>{
     if (statePause){
       state = true;
-  
+      
       clearInterval(myInterval);
+
+      pomoCat.src = "./assets/pomocat-pause.png"
   }
   console.log("Pausado");
 
@@ -72,32 +103,101 @@ pauseBtn.addEventListener("click", appPause);
 
 // Reset Button
 
-
-
 const appReset = () =>{
   if (stateReset){
     state = true;
 
     clearInterval(myInterval);
 
-    minuteDiv.textContent = minutesDefault
-    secondDiv.textContent = secondsDefault
+    minuteDiv.textContent = minutesDefault;
+    secondDiv.textContent = secondsDefault;
 
-    totalSeconds = sessionAmount * 60;
-
+    totalSeconds = Number.parseInt(minuteDiv.textContent) * 60;
+    pomoCat.src = "./assets/pomocat-pause.png"
+    
   }
 
 }
 
 resetBtn.addEventListener("click", appReset);
 
+//Pomodoro
+
+const pomodoroTimer = () =>{
+  if(pomodoro){
+    state = true;
+    gifActive = false;
+    gifBreak = true;
+
+    clearInterval(myInterval);
+
+    minutesDefault = "25";
+    secondsDefault = "00";
+
+    minuteDiv.textContent = minutesDefault;
+    secondDiv.textContent = secondsDefault;
+
+    totalSeconds=sessionAmount * 60;
+
+    pomoCat.src = "./assets/pomocat-pause.png"
+
+  }
+
+}
+
+pomodoroBtn.addEventListener("click", pomodoroTimer);
 
 
+//Short Break
 
+const shortBreakTimer = () =>{
+  if(shortBreak){
+    state=true;
+    gifBreak = false;
+    gifActive = true;
 
+    clearInterval(myInterval);
 
+    minutesDefault = "5";
+    secondsDefault = "00";
 
+    minuteDiv.textContent = minutesDefault;
+    secondDiv.textContent = secondsDefault;
 
+    const sessionShortBreakAmount = Number.parseInt(minuteDiv.textContent);
+    totalSeconds = sessionShortBreakAmount * 60;
 
+    pomoCat.src = "./assets/pomocat-pause.png"
 
+  } 
 
+}
+
+shortBreakBtn.addEventListener("click", shortBreakTimer);
+
+//Long Break
+
+const longBreakTimer = () =>{
+  if(longBreak){
+    state=true;
+    gifBreak = false;
+    gifActive = true;
+
+    clearInterval(myInterval);
+
+    minutesDefault = "15";
+    secondsDefault = "00";
+
+    minuteDiv.textContent = minutesDefault;
+    secondDiv.textContent = secondsDefault;
+
+    const sessionLongBreakAmount = Number.parseInt(minuteDiv.textContent);
+    totalSeconds = sessionLongBreakAmount * 60;
+
+    pomoCat.src = "./assets/pomocat-pause.png"
+
+  } 
+
+}
+
+longBreakBtn.addEventListener("click", longBreakTimer);
